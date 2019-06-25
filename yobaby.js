ae4aa6da8a15d56070d0bcb117f120bfbade9d00
Chris <50125876+SQUADBCHRIS@users.noreply.github.com>
2019-06-25 11:01:22 -0700

    Add files via upload
    just code of me following along and doing assignments from the colt steele web dev course.

---
+++ yobaby.js
@@ -0,0 +1,62 @@
+/*var userFirst = prompt("what is your first name?")
+var userLast = prompt("what is your last name?")
+var guessAge = prompt("what is your age?")
+alert("youve been alive " + guessAge * 365 + " days")
+console.log(userFirst, userLast)
+console.log(guessAge)
+
+var entryAge = prompt("how old are you?")
+
+if(entryAge < 0){
+    console.log("your age is negative");
+}
+if(Number(entryAge) === 21){
+    console.log("come drink bruh!!!");
+}
+if(entryAge % 2 !== 0) {
+    console.log("your age is odd");
+}
+if(Number(entryAge) % Math.sqrt(entryAge)){
+     console.log("PERFECT SQUARE!");
+}/*
+   
+//var num = -10;
+
+//while(num <= 19){
+  //  console.log(num);
+    //num++;
+//}
+
+/*var num = 10;
+
+while(num <= 40){
+    console.log(num);
+    num+=2;
+}*/
+
+
+/*var num = 300;
+
+  while (num <= 333){
+  if (num % 2 !== 0){
+     console.log(num);
+     }
+     num+= 1;
+     }*/
+
+
+  /*var numm = 5;
+
+  while (numm <= 50) {
+
+if (numm % 5 === 0 && numm  % 3 === 0){
+     console.log(numm);
+     }
+     numm+= 1;
+  }*/
+
+  
+
+
+ 
+ 
